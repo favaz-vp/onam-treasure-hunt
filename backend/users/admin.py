30853da2e8d439309ffe776cd7938b4a9275324f
ivin <ivinstephen001@gmail.com>
2026-08-14 14:45:29 +0530

    updates

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -12,7 +12,20 @@ class UserAdmin(BaseUserAdmin):
     list_filter = ('is_staff', 'is_superuser', 'is_captain')
     search_fields = ('email',)
     ordering = ('email',)
-    filter_horizontal = ()
+    fieldsets = (
+    (None, {"fields": ("email", "password")}),
+    ("Personal info", {"fields": ("first_name", "last_name")}),
+    ("Permissions", {
+        "fields": (
+            "is_active",
+            "is_staff",
+            "is_superuser",
+            "groups",
+            "user_permissions",
+        ),
+    }),
+    ("Important dates", {"fields": ("last_login", "date_joined")}),
+)
 
 class UserInline(admin.TabularInline):
     model = User
